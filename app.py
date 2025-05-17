2f488bec198cbf8e3d9ae149d2ed9850a9912882
brahim764 <brahimsabri406@gmail.com>
2025-05-16 22:32:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
 from fastapi import FastAPI, Request
-from vocode.streaming.telephony.twilio_signal import TwilioSignalHandler
-from vocode.streaming.agent.simple_agent import SimpleAgent
-from vocode.streaming.models.telephony import TwilioConfig
-from vocode.streaming.input.twilio_input import TwilioInputAudioConfig
-from vocode.streaming.output.eleven_labs_output import ElevenLabsOutputAudioConfig
 import os
+from vocode.streaming.telephony.server.base import TwilioInboundCallConfig
+from vocode.streaming.telephony.server.twilio.twilio_phone_call_router import TwilioPhoneCallRouter
+from vocode.streaming.agent.simple_agent import SimpleAgent
+from vocode.streaming.input_device.twilio_input import TwilioInputAudioConfig
+from vocode.streaming.output_device.eleven_labs_output import ElevenLabsOutputAudioConfig
+from vocode.streaming.models.synthesizer import ElevenLabsSynthesizerConfig
+from vocode.streaming.models.transcriber import TwilioTranscriberConfig
+from vocode.streaming.models.telephony import TwilioConfig
 
 app = FastAPI()
 
-@app.post("/twilio")
-async def twilio_webhook(request: Request):
-    handler = TwilioSignalHandler(
-        agent=SimpleAgent(
-            prompt_preamble="Bonjour, ici la Clinique Dentaire Laurier. Voulez-vous prendre un rendez-vous, en modifier un ou poser une question ?"
-        ),
-        twilio_config=TwilioConfig(
-            account_sid=os.environ["TWILIO_ACCOUNT_SID"],
-            auth_token=os.environ["TWILIO_AUTH_TOKEN"],
-            phone_number=os.environ["TWILIO_PHONE_NUMBER"],
-        ),
-        input_audio_config=TwilioInputAudioConfig(),
-        output_audio_config=ElevenLabsOutputAudioConfig(
+twilio_router = TwilioPhoneCallRouter(
+    agent_thunk=lambda: SimpleAgent(
+        prompt_preamble="Bonjour, ici la Clinique Dentaire Laurier. Voulez-vous prendre un rendez-vous, en modifier un ou poser une question ?"
+    ),
+    twilio_config=TwilioConfig(
+        account_sid=os.environ["TWILIO_ACCOUNT_SID"],
+        auth_token=os.environ["TWILIO_AUTH_TOKEN"],
+        phone_number=os.environ["TWILIO_PHONE_NUMBER"]
+    ),
+    input_audio_config=TwilioInputAudioConfig(),
+    output_audio_config=ElevenLabsOutputAudioConfig(
+        config=ElevenLabsSynthesizerConfig(
             api_key=os.environ["ELEVENLABS_API_KEY"],
             voice_id=os.environ["ELEVENLABS_VOICE_ID"]
         )
-    )
-    return await handler.handle(request)
+    ),
+    transcriber_config=TwilioTranscriberConfig(),
+)
+
+@app.post("/twilio")
+async def twilio(request: Request):
+    body = await request.body()
+    return await twilio_router.handle_request(body)
