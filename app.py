2642d5816d3b5f683d173028d065104e5238da9b
brahim764 <brahimsabri406@gmail.com>
2025-05-16 21:43:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from vocode.streaming.telephony.twilio_signal import TwilioSignalHandler
+from vocode.streaming.telephony.twilio_voice_gateway import TwilioVoiceGateway
 from vocode.streaming.agent.simple_agent import SimpleAgent
 from vocode.streaming.output.eleven_labs_output import ElevenLabsOutputAudioConfig
 from vocode.streaming.input.twilio_input import TwilioInputAudioConfig
@@ -8,21 +8,20 @@ import os
 
 app = FastAPI()
 
-@app.post("/twilio")
-async def twilio_webhook(request: Request):
-    handler = TwilioSignalHandler(
-        agent=SimpleAgent(
-            prompt_preamble="Bonjour ! Ici la Clinique Dentaire Laurier. Souhaitez-vous prendre un rendez-vous, en modifier un, ou poser une question ?"
-        ),
-        twilio_config=TwilioConfig(
-            account_sid=os.environ["TWILIO_ACCOUNT_SID"],
-            auth_token=os.environ["TWILIO_AUTH_TOKEN"],
-            phone_number=os.environ["TWILIO_PHONE_NUMBER"],
-        ),
-        input_audio_config=TwilioInputAudioConfig(),
-        output_audio_config=ElevenLabsOutputAudioConfig(
-            api_key=os.environ["ELEVENLABS_API_KEY"],
-            voice_id=os.environ["ELEVENLABS_VOICE_ID"]
-        ),
-    )
-    return await handler.handle_signal(request)
+voice_gateway = TwilioVoiceGateway(
+    agent=SimpleAgent(
+        prompt_preamble="Bonjour ! Ici la Clinique Dentaire Laurier. Souhaitez-vous prendre un rendez-vous, en modifier un, ou poser une question ?"
+    ),
+    twilio_config=TwilioConfig(
+        account_sid=os.environ["TWILIO_ACCOUNT_SID"],
+        auth_token=os.environ["TWILIO_AUTH_TOKEN"],
+        phone_number=os.environ["TWILIO_PHONE_NUMBER"],
+    ),
+    input_audio_config=TwilioInputAudioConfig(),
+    output_audio_config=ElevenLabsOutputAudioConfig(
+        api_key=os.environ["ELEVENLABS_API_KEY"],
+        voice_id=os.environ["ELEVENLABS_VOICE_ID"]
+    ),
+)
+
+app.include_router(voice_gateway.get_router(), prefix="/twilio")
