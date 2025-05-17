9313f481eb29595569f2d1f78c198c4cc3ac7f7e
brahim764 <brahimsabri406@gmail.com>
2025-05-16 22:16:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 from fastapi import FastAPI, Request
 from vocode.streaming.telephony.twilio_handler import TwilioPhoneCallHandler
 from vocode.streaming.agent.simple_agent import SimpleAgent
 from vocode.streaming.models.telephony import TwilioConfig
-from vocode.streaming.output.eleven_labs_output import ElevenLabsOutputAudioConfig
 from vocode.streaming.input.twilio_input import TwilioInputAudioConfig
+from vocode.streaming.output.eleven_labs_output import ElevenLabsOutputAudioConfig
 import os
 
 app = FastAPI()
@@ -18,13 +17,12 @@ async def twilio_webhook(request: Request):
         twilio_config=TwilioConfig(
             account_sid=os.environ["TWILIO_ACCOUNT_SID"],
             auth_token=os.environ["TWILIO_AUTH_TOKEN"],
-            phone_number=os.environ["TWILIO_PHONE_NUMBER"]
+            phone_number=os.environ["TWILIO_PHONE_NUMBER"],
         ),
         input_audio_config=TwilioInputAudioConfig(),
         output_audio_config=ElevenLabsOutputAudioConfig(
             api_key=os.environ["ELEVENLABS_API_KEY"],
             voice_id=os.environ["ELEVENLABS_VOICE_ID"]
-        ),
+        )
     )
-
     return await handler.handle(request)
